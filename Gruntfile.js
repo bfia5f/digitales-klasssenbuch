24bfeeeebd6251dd471ebfe8bb0355bd9eabe67a
Tobias Stosius <stosius.tobias@gmail.com>
2017-02-02 00:30:53 +0100

    [TASK] Add more Grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,57 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		// running `grunt less` will compile once
-		less: {
-			development: {
-				options: {
-					paths: ["public/css"],
-				},
-			files: {
-				"public/css/main.css": "less/main.less"
-			}
-		}
-	},
-	// running `grunt watch` will watch for changes
-	watch: {
-		files: "less/*.less",
-		tasks: ["less"]
-	}
-});
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['private/javascript/**/*.js'],
+                dest: 'public/javascript/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'public/javascript/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        less: {
+            dev: {
+                files: {
+                    'public/css/main.css': 'private/less/main.less'
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'private/javascript/**/*.js', 'test/**/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>', 'private/less/*.less'],
+            tasks: ['jshint', 'less']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['jshint', 'less', 'concat', 'uglify']);
+
 };
